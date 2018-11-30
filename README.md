90603b5521f6cecb2d52cf85d88ff21e177c5737
Max Wolf <maxcharleswolf@gmail.com>
2018-11-29 21:31:04 -0600

    figured out a stupid project idea; added login and image upload; readme edit

--- README.md
+++ README.md
@@ -1,29 +1,39 @@
-# aws-amplify-vue-test
+# Celery Memes
+
+The best place to upload Celery Man, Tayne, Oyster, and Mozzarell media on the web!
+
+> It's actually just a fun exploration of AWS-Amplify, Vue, and Element UI
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn run build
 ```
 
 ### Run your tests
+
 ```
 yarn run test
 ```
 
 ### Lints and fixes files
+
 ```
 yarn run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to My Shitty Vue-Amplify Test App" />
-    <div v-if="!signedIn"><amplify-authenticator></amplify-authenticator></div>
-    <div v-if="signedIn"><amplify-sign-out></amplify-sign-out></div>
+    <div class="banner"></div>
+    <!-- <img src="./assets/celeryman-warning-nsfw.png"> -->
+    <HelloWorld msg="CELERY MEMES"/>
+    <div v-if="!signedIn">
+      <amplify-authenticator></amplify-authenticator>
+    </div>
+    <div v-if="signedIn">
+      <amplify-sign-out class="signout"></amplify-sign-out>
+      <div class="container">
+        <el-button type="primary" @click="showAlbum()">Add New Celery Meme</el-button>
+        <amplify-s3-album path="images/"></amplify-s3-album>
+        <amplify-photo-picker v-if="albumShown" v-bind:photoPickerConfig="photoPickerConfig"></amplify-photo-picker>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +21,11 @@
 import HelloWorld from "./components/HelloWorld.vue";
 import { AmplifyEventBus } from "aws-amplify-vue";
 import { Auth } from "aws-amplify";
+
+const photoPickerConfig = {
+  path: "images/"
+};
+
 export default {
   name: "app",
   components: {
@@ -33,20 +48,43 @@ export default {
   },
   data() {
     return {
+      photoPickerConfig,
       signedIn: false,
-      fuckLinter: user
+      albumShown: true
     };
+  },
+  methods: {
+    showAlbum() {
+      albumShown ? (albumShown = false) : (albumShown = true);
+    }
   }
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  padding: 40px;
+}
+.signout {
+  background-color: #ededed;
+  margin: 0;
+  padding: 11px 0px 1px;
+}
+.banner {
+  width: 100%;
+  background-image: url(./assets/tayne-triple.jpg);
+  height: 250px;
+  background-color: purple;
+  background-position: top;
 }
 </style>
